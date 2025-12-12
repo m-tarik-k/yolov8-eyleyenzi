8c5dd143d0a21d78b439b38eda694f275dc63a33
m-tarik-k <m.tarik.karatas@gmail.com>
2025-12-12 14:28:58 +0300

    ...

--- Projects/GoogleColab/YOLOxDETR_Karsilastiri.py
+++ Projects/GoogleColab/YOLOxDETR_Karsilastiri.py
@@ -6,7 +6,6 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-import torch
 from ultralytics import YOLO
 from rfdetr.detr import RFDETRSmall, RFDETRMedium, RFDETRLarge
 
